8cdf8e489c13016cbea10b7dd5446f2583d858fb
Mehul Tahiliani <53617853+DaDevFox@users.noreply.github.com>
2026-09-13 14:17:23 -0700

    DEL_(XXXX): unused param

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -5,18 +5,14 @@ import (
 	"testing"
 )
 
-func CustomEfficiencyStats(name string, serial func(*testing.B), parallel func(*testing.B, int), threadCounts []int) {
-	// fmt.Printf("%s/Serial\n", name)
+func CustomEfficiencyStats(serial func(*testing.B), parallel func(*testing.B, int), threadCounts []int) {
 	serialB := testing.Benchmark(serial)
-	// fmt.Println(serialB.String())
 
 	for _, threadCount := range threadCounts {
 
-		// fmt.Printf("%s/Parallel[threadCount=%d]\n", name, threadCount)
 		parallelB := testing.Benchmark(func(b *testing.B) {
 			parallel(b, threadCount)
 		})
-		// fmt.Println(parallelB.String())
 
 		speedup := float32(serialB.NsPerOp()) / float32(parallelB.NsPerOp())
 		maxCost := float32(parallelB.NsPerOp() * int64(threadCount)) // TODO: ideally use sum thread wall time for this metric
